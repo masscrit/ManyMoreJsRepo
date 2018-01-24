1babcfb000905295a8a8f4dd47103eb0036f1e8a
Vollmilch <vollmilch-ownzu@gmx.net>
2018-01-24 21:10:31 +0100

    push

--- src/main/java/de/vollmilch/manymorejs/core/TestApplication.java
+++ src/main/java/de/vollmilch/manymorejs/core/TestApplication.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@Component
 public class TestApplication {
 
 	/** logger */
